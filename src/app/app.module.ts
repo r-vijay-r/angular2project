e77db7591bdf0586a6a3d3765b78e2b5dedb14cd
r-vijay-r <503r.vijay@gmail.com>
2016-10-03 17:28:01 +0530

    changes made for firebase login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
+import { AuthProviders,AuthMethods} from 'angularfire2';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyDnrhypbB6y8FUYJAClANT7EYHqNJFtd0c",
+ authDomain: "ng2fire-daeba.firebaseapp.com",
+ databaseURL: "https://ng2fire-daeba.firebaseio.com/",
+ storageBucket: "ng2fire-daeba.appspot.com"
+}
+const AuthenticationConfig = {
+   method: AuthMethods.Popup,
+   provider: AuthProviders.Google
+ }
+AngularFireModule.initializeApp(firebaseConfig, AuthenticationConfig)
 
 @NgModule({
   declarations: [
